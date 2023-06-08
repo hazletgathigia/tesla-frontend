c47b46e962040a8b3f769bbed3a97633893a10a1
hazlet <hazletgathigia@gmail.com>
2023-06-08 13:07:52 +0200

    reverted to default BrowserRouter

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { store } from "./_service/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <BrowserRouter basename="/tesla-frontend">
+        <BrowserRouter>
             <Provider store={store}>
                 <App />
             </Provider>
