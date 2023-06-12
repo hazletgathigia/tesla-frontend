a38cb2da2e3becb55825e5b84be54b0283239a08
hazlet <hazletgathigia@gmail.com>
2023-06-12 12:07:22 +0200

    set up for netlify deployment from gitlab

--- src/index.js
+++ src/index.js
@@ -10,10 +10,10 @@ import { store } from "./_service/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <HashRouter>
+        <BrowserRouter>
             <Provider store={store}>
                 <App />
             </Provider>
-        </HashRouter>
+        </BrowserRouter>
     </React.StrictMode>
 );
