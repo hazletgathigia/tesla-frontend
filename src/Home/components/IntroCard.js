d43b064dbcdd83097ade1e8cee2b25c96d738e45
hazlet <hazletgathigia@gmail.com>
2023-06-13 16:14:19 +0200

    added homescreen css

---
+++ src/Home/components/IntroCard.js
@@ -0,0 +1,16 @@
+import { Card, CardBody, CardTitle } from "reactstrap";
+
+const IntroCard = () => {
+    return (
+        <>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus at sagittis mi, sit amet semper massa.
+            Phasellus et rutrum augue. Aliquam eget urna sit amet lorem mattis lobortis eu sit amet mi. Cras id enim
+            augue. Morbi lacinia ante id dignissim lacinia. Nulla vestibulum urna nec diam placerat ullamcorper.
+            Phasellus tempor sapien in justo euismod pulvinar. Vestibulum mattis ante sem. Donec augue neque, faucibus
+            venenatis mattis sed, egestas iaculis tortor. Fusce nec pretium urna. Quisque sit amet mauris ut eros
+            feugiat accumsan.
+        </>
+    );
+};
+
+export default IntroCard;
--- src/Home/screens/HomeScreen.js
+++ src/Home/screens/HomeScreen.js
@@ -2,12 +2,24 @@ import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { fetchIdData } from "../../_service/actions";
 import DataCollectionForm from "../components/DataCollectionForm";
+import IntroCard from "../components/IntroCard";
+import "./HomeScreen.css";
 
 const HomeScreen = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
-    return <DataCollectionForm handleSubmit={handleSubmit} />;
+    return (
+        <div className="HomeScreen">
+            <div className="Title">Ixxat's USB-TO-CAN File Processor</div>
+            <div className="IntroCard">
+                <IntroCard />
+            </div>
+            <div className="FileUploadForm">
+                <DataCollectionForm className="Fileform" handleSubmit={handleSubmit} />
+            </div>
+        </div>
+    );
 
     function handleSubmit(data) {
         const formData = createFormData(data);
